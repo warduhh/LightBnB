d80f765c81613a799cf2c55b93cf75c842797fda
Warda Nur <warduhh@gmail.com>
2023-04-24 02:47:27 +0000

    updated add properties

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -18,14 +18,18 @@ const users = require("./json/users.json");
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+return pool
+    .query(`SELECT * FROM users WHERE email = $1;`,[email])
+    .then((res) => {
+      console.log(res.rows);
+      if(!res.rows[0]){
+        return null;
+      }
+      return res.rows[0];
+    })
+    // .catch((error) => {
+    //   console.log(error.message);
+    // })
 };
 
 /**
@@ -34,19 +38,31 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1;`,[id])
+  .then((res) => {
+    console.log(res.rows);
+    if(!res.rows[0]){
+      return null;
+    }
+    return res.rows[0];
+  })
+  // .catch((error) => {
+  //   console.log(error.message);
+  // })
 };
-
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query('INSERT INTO users (name, email, password) VALUES($1, $2, $3) RETURNING *',[user.name, user.email, user.password] )
+    .then((res) =>{
+      console.log(res.rows);
+    })
+
 };
 
 /// Reservations
@@ -69,15 +85,17 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-  pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
+
+  return pool
+     .query(`SELECT * FROM properties LIMIT $1`, [limit])
+     .then((result) => {
+       //console.log(result.rows);
+       return result.rows;
+     })
+     .catch((err) => {
+       console.log(err.message);
+     });
+ };
 
 /**
  * Add a property to the database
